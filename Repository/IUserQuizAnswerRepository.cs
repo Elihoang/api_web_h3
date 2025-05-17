f1b80c98552905c8813177e990902211a6f33a74
Le-Hoai-Huan <149983414+Le-Hoai-Huan@users.noreply.github.com>
2025-05-17 07:34:13 +0700

    done quiz

--- Repository/IUserQuizAnswerRepository.cs
+++ Repository/IUserQuizAnswerRepository.cs
@@ -8,4 +8,7 @@ public interface IUserQuizAnswerRepository
     Task<UserQuizAnswer> GetByIdAsync(Guid id);
     Task<IEnumerable<UserQuizAnswer>> GetByUserIdAsync(Guid userId);
     Task<IEnumerable<UserQuizAnswer>> GetByQuizIdAsync(string quizId);
+    Task<UserQuizAnswer> UpdateAsync(UserQuizAnswer userAnswer);
+    Task<IEnumerable<UserQuizAnswer>> GetByLessonIdAsync(string lessonId, Guid userId);
+    Task<bool> DeleteAsync(string id);
 }
